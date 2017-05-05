db64a2f4b72330efd278080a297518075a1dee4d
Pedro Bahamondes <pedro.bahamondes@polytechnique.edu>
2017-05-05 16:22:33 +0200

    Only the biggest visage is shown in a rectangle

--- scripts/fd.py
+++ scripts/fd.py
@@ -10,7 +10,6 @@ import time
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
-import threading
 
 class face_det:
 
@@ -18,17 +17,19 @@ class face_det:
     self.image_pub = rospy.Publisher("image_topic_out",Image, queue_size=1)
     self.bridge = CvBridge()
     self.image_sub = rospy.Subscriber("image_topic_in",Image,self.callback)
-    #self.lock = threading.RLock()
     # Create the haar cascade
     cascPath = "/home/turtlebot/catkin_ws/src/tp_ros_ensta_u2is/scripts/haarcascade_frontalface_default.xml"
     self.faceCascade = cv2.CascadeClassifier(cascPath)
+    # Bigger face
+    self.h = -1
+    self.x = -1
+    self.y = -1
+    self.w = -1
 
 
   # Callback when a new image arrived
   def callback(self,data):
-    #self.lock.acquire()
     # Read the image
-    
     try:
       image = self.bridge.imgmsg_to_cv2(data, "bgr8")
     except CvBridgeError as e:
@@ -42,21 +43,23 @@ class face_det:
       minSize=(120, 110)
       #flags = cv2.CV_HAAR_SCALE_IMAGE
     )
-    
-
     #print("Found {0} faces!".format(len(faces)))
-
+    self.h = -1
+    self.x = -1
+    self.y = -1
+    self.w = -1
     # Draw a rectangle around the faces
     for (x, y, w, h) in self.faces:
-      cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+      if h > self.h:
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+    if(self.x >= 0):
+      cv2.rectangle(image, (self.x, self.y), (self.x+self.w, self.y+self.h), (0, 255, 0), 2)
 
     cv2.imshow("Faces found", image)
     cv2.waitKey(1)
-    '''try:
-      self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
-    except CvBridgeError as e:
-      print(e)'''
-    #self.lock.release()
 
 def main(args):
   print("OpenCV Version: {}".format(cv2.__version__))
